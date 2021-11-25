ebd392eaf6b74c6350071dcf495c77179806f624
Enigmatisms <984041003@qq.com>
2021-11-25 13:48:08 +0800

    Tranfer to RTX 3060 for training.

--- train.py
+++ train.py
@@ -11,14 +11,14 @@ from torch.autograd import Variable as Var
 from torchvision.utils import save_image
 from torchvision.datasets import CIFAR10
 
-from CCT import CCT
+from py.CCT import CCT
 
 from torch import optim
 from torch import nn
 from torchvision import transforms
 from torch.utils.data.dataloader import DataLoader
 
-from train_utils import *
+from py.train_utils import *
 
 load_path = ""
 default_chkpt_path = "./check_points/"
@@ -26,7 +26,7 @@ default_model_path = "./model/"
 
 # Calculate accurarcy (correct prediction counter)
 def accCounter(pred:torch.FloatTensor, truth:torch.FloatTensor)->int:
-    return 0
+    pass
 
 if __name__ == "__main__":
 
@@ -54,12 +54,6 @@ if __name__ == "__main__":
 
     to_tensor = transforms.ToTensor()
     
-    if args.load:
-        if os.path.exists(load_path) == False:
-            print("No such file as '%s'! Model failed to load."%(load_path))
-        else:
-            pass
-            # model.loadFromFile(load_path)
     device = torch.device(type = 'cpu')
     if use_cuda and torch.cuda.is_available():
         device = torch.device(0)
@@ -72,8 +66,6 @@ if __name__ == "__main__":
     train_set = getCIFAR10Dataset(True, to_tensor, batch_size)
     test_set = getCIFAR10Dataset(False, to_tensor, batch_size)
     
-    # ------------ model --------------
-    # TODO: ??import???????
     model:CCT = CCT()       # default parameters
     if use_load == True and os.path.exists(load_path):
         model.loadFromFile(load_path)
@@ -85,18 +77,19 @@ if __name__ == "__main__":
     opt_sch = optim.lr_scheduler.MultiStepLR([15, 30, 45, 60], gamma = 0.1, last_epoch = -1)
 
     batch_num = len(train_set) // batch_size
+    train_cnt = 0
     for ep in range(epochs):
         model.train()
-        train_cnt = 0
         train_acc_cnt = 0
+        train_num = 0
         for i, px, py in enumerate(train_set):
-            train_cnt += 1
-            loss = torch.zeros(1).to(device)
+            loss:torch.Tensor = torch.zeros(1).to(device)
             px:torch.Tensor = px.to(device)
             py:torch.Tensor = py.to(device)
             pred = model(px)
             loss = loss_func(pred, py)
             train_acc_cnt += accCounter(pred, py)
+            train_num += len(pred)
             if train_cnt % eval_time == 1:
                 with torch.no_grad():
                     model.eval()
@@ -110,31 +103,28 @@ if __name__ == "__main__":
                         pred = model(ptx)
                         test_acc_cnt += accCounter(pred, pty)
                         test_loss += loss_func(pred, pty)
-                    train_acc = train_acc_cnt / train_cnt
+                    train_acc = train_acc_cnt / train_num
                     test_acc = test_acc_cnt / test_length
-                    print("Epoch: %3d / %3d\t Batch %4d / %4d\t train loss: %.4f\t test loss: %.4f\t acc: %.4f\t test acc: %.4f\t lr: %f"%(
+                    print("Epoch: %4d / %4d\t Batch %4d / %4d\t train loss: %.4f\t test loss: %.4f\t acc: %.4f\t test acc: %.4f\t lr: %f"%(
                             ep, epochs, i, batch_num, loss.item(), test_loss.item(), train_acc, test_acc, opt_sch.param_groups[0]['lr']
                     ))
-                    train_cnt = 0
-                    # writer.add_scalar('Loss/Train Loss', loss, train_cnt)
-                    # writer.add_scalar('Loss/Test loss', test_loss, train_cnt)
-                    # writer.add_scalar('Acc/Train Set Accuracy', train_acc, train_cnt)
-                    # writer.add_scalar('Acc/Test Set Accuracy', test_acc, train_cnt)
+                    train_num = 0
+                    writer.add_scalar('Loss/Train Loss', loss, train_cnt)
+                    writer.add_scalar('Loss/Test loss', test_loss, train_cnt)
+                    writer.add_scalar('Acc/Train Set Accuracy', train_acc, train_cnt)
+                    writer.add_scalar('Acc/Test Set Accuracy', test_acc, train_cnt)
             if train_cnt % chkpt_ntv == 0:
-                # torch.save({
-                #     'model': model.state_dict(),
-                #     'optimizer': opt.state_dict()},
-                #     "%schkpt_%d.pt"%(default_chkpt_path, train_cnt)
-                # )
-                pass
-            
-        # For recontruction task:
-        # save_image(gen.detach().clamp_(0, 1), "..\\imgs\\G_%d.jpg"%(epoch + 1), 1)
-    # torch.save({
-    #     'model': model.state_dict(),
-    #     'optimizer': opt.state_dict()},
-    #     "%s/model_1.pth"%(default_model_path)
-    # )
+                torch.save({
+                    'model': model.state_dict(),
+                    'optimizer': opt.state_dict()},
+                    "%schkpt_%d.pt"%(default_chkpt_path, train_cnt)
+                )
+            train_cnt += 1
+    torch.save({
+        'model': model.state_dict(),
+        'optimizer': opt.state_dict()},
+        "%s/model_1.pth"%(default_model_path)
+    )
     writer.close()
     print("Output completed.")
     
