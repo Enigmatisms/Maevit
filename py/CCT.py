edaf347353cad6788e52303417a2f63b78ce6b87
Enigmatisms <984041003@qq.com>
2021-11-28 00:00:37 +0800

    Merged conflicts.

--- py/CCT.py
+++ py/CCT.py
@@ -53,30 +53,18 @@ class CCT(nn.Module):
         self.sp = SeqPool(emb_dim)
         self.norm = nn.LayerNorm(emb_dim)
         self.classify = nn.Linear(emb_dim, 10)
-<<<<<<< HEAD
         self.flatten = nn.Flatten(2, 3)
         for i in range(self.conv_num):
             self.conv[i].apply(self.init_weight)
-=======
->>>>>>> 8f8d0588e20e9b3d3e3e134c248790019a050532
         
     def forward(self, X:torch.Tensor):
         for i in range(self.conv_num):
             X = self.conv[i](X)
         # reshape convolution outputs: (n, emb_dim, w0, h0)
-<<<<<<< HEAD
         X = self.flatten(X).transpose(-1, -2)
         X = self.transformers(X)
         # The problem is: LayerNorm? When to norm? Whether to norm？
         z = self.norm(X)
         z = self.sp(z).squeeze(dim = 1)
         return self.classify(z)
-=======
-        batch_size, emb_dim, _, _ = X.shape
-        X = X.view(batch_size, -1, emb_dim)     # (n, seq_length=w0 * h0, emb_dim)
-        X = self.transformers(X)
-        z = self.sp(X)
-        z = self.norm(z)
-        return self.classify(z).squeeze(dim = 1)
->>>>>>> 8f8d0588e20e9b3d3e3e134c248790019a050532
         
